67e2ba131bc1bbf354e38822dc00b3965f07faa9
profissionalGuilhermeVitor <profissionalGuilhermeVitor@gmail.com>
2026-02-05 12:50:29 -0300

    (05/02) Criando a API de conexão

--- databases/run_sql.py
+++ databases/run_sql.py
@@ -1,32 +1,23 @@
-# Módulo de conexão ao banco de dados
-
-# pip install psycopg2
-# https://pypi.org/project/psycopg2/
-
-# Imports
 import os
 import psycopg2
 import psycopg2.extras as ext
 
-# Função
 def run_sql(sql, values = None):
 
-    # Variáveis de controle
     conn = None
     results = []
-
-    # Conexão ao banco de dados
     try:
-        conn = psycopg2.connect("host=localhost port=5432 dbname=dbapp user=postgres password=dsacademy")
-        cur = conn.cursor(cursor_factory = ext.DictCursor)
-        cur.execute(sql, values)
+        conn = psycopg2.connect("host=localhost port=5432 dbname=dbapp user=postgrees password=dsacademy")
+        cursor = conn.cursor(cursor_factory= ext.DictCursor)
+        cursor.execute(sql, values)
         conn.commit()
-        results = cur.fetchall()
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
+        results = cursor.fetchall()
+        cursor.close()
+    except(Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
-        if conn is not None:
+        if conn.close is not None:
             conn.close()
-            
+    
     return results
+
